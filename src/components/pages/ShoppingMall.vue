a2e9deca0a952005a0ce17954f6fc935deb51ea4
Jeckhenry <571977485@qq.com>
2018-05-09 20:58:23 +0800

    首次提交

---
+++ src/components/pages/ShoppingMall.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <van-row class="test-row">
+            <van-col span='8'>
+                span:8
+            </van-col>
+            <van-col span='8'>
+                span:8
+            </van-col>
+            <van-col span='8'>
+                span:8
+            </van-col>
+        </van-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                msg:"shoppingmall"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .test-row{
+        text-align: center;
+    }
+</style>
